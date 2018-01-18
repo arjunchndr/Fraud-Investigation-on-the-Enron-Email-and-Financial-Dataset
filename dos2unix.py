c96cdda243f67a51873d0f1669d8b8154fa457a3
Arjun Chandra <arjun.chndr@gmail.com>
2018-01-18 13:55:15 -0500

    Update dos2unix.py

--- dos2unix.py
+++ dos2unix.py
@@ -3,8 +3,8 @@
 convert dos linefeeds (crlf) to unix (lf)
 usage: dos2unix.py 
 """
-original = "final_project_dataset.pkl"
-destination = "final_project_dataset_unix.pkl"
+original = "Enron_dataset.pkl"
+destination = "Enron_dataset_unix.pkl"
 
 content = ''
 outsize = 0
